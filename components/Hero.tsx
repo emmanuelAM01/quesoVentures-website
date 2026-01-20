efd60c0b136efce1695c6a6ff76f847f2941fbd3
Emmanuel Mendieta <emman0621@gmail.com>
2026-01-19 18:39:00 -0600

    prices

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,24 +3,18 @@ import Link from "next/link";
 export default function Hero() {
   return (
     <section className="text-center flex flex-col items-center justify-center mx-4 sm:mx-8 md:mx-16 lg:mx-24 min-h-screen">
-      <h1 className="font-sans text-3xl tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl dark:text-white mb-6">
-        Your app ideas and website goals are within reach
+      <h1 className="font-sans text-xl tracking-tighter sm:text-2xl md:text-3xl lg:text-5xl dark:text-white mb-6">
+        Have any tech stuff you need done?
       </h1>
-      <p className="text-xl text-gray-600 dark:text-gray-300 mb-12 font-light">
-        The technical business partner you never had
+      <p className="text-2xl text-gray-600 dark:text-gray-300 mb-12 font-light">
+        Websites and Apps built/fixed fast.
       </p>
       <div className="flex justify-center space-x-6">
         <Link
-          href="/get-started"
+          href="anchor tab to PriceEstimator"
           className="bg-black text-white dark:bg-white dark:text-black px-5 py-2 rounded-md text-base font-semibold hover:bg-gray-800 dark:hover:bg-gray-200 transition duration-300"
         >
-          Get Started
-        </Link>
-        <Link
-          href="https://github.com"
-          className="bg-black text-white dark:bg-white dark:text-black px-5 py-2 rounded-md text-base font-semibold hover:bg-gray-800 dark:hover:bg-gray-200 transition duration-300"
-        >
-          Download
+          Get a Quote
         </Link>
       </div>
     </section>
