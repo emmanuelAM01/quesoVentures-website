276c72e7a3fef159176cbb1e3fbd72f9c8bcdea3
Emmanuel M <emman0621@gmail.com>
2026-01-29 20:10:50 -0600

    feat: preload image and hostname for mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preload" href="/about.JPEG" as="image" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter+Tight:ital,wght@0,100..900;1,100..900&display=swap"
