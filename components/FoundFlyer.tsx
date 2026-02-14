77f397cee99e006cd12f47686b5d9777fa750e83
Emmanuel Mendieta <emman0621@gmail.com>
2026-02-13 18:29:18 -0600

    update: found code text

--- components/FoundFlyer.tsx
+++ components/FoundFlyer.tsx
@@ -4,16 +4,16 @@ export default function Hero() {
   return (
     <section className="text-center flex flex-col items-center justify-center mx-4 sm:mx-8 md:mx-16 lg:mx-24 min-h-screen">
       <h1 className="font-sans text-2xl tracking-tight sm:text-3xl md:text-4xl lg:text-5xl text-lightText dark:text-darkText mb-6">
-        So, you found a QR code, that means I went to your business and think I can help you out technically.
+        So, you found the QR code. I can help you grow your business.
       </h1>
 
       <p className="text-xl text-lightTextMuted dark:text-darkTextMuted mb-12 font-light max-w-2xl">
-        Helping local businesses get and stay online so you can focus doing what you do best.
+        I leave my stamps where I know I can help
       </p>
 
       <div className="flex justify-center">
         <Link
-          href="/#pricing-estimator"
+          href="/"
           className="
               bg-lightButton hover:bg-lightButtonHover dark:bg-darkButton dark:hover:darkButtonHover
               text-lightBG dark:text-darkBG
@@ -23,7 +23,7 @@ export default function Hero() {
               transition-colors
             "
         >
-          Get a website quote
+          Learn More
         </Link>
       </div>
     </section>
